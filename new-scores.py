6105f263077fa88828517b3b1632ab8d2707b4f8
Purva <thereconpilot@gmail.com>
2021-06-28 01:22:56 +0530

    Complete Scoring Function

--- new-scores.py
+++ new-scores.py
@@ -1,51 +1,18 @@
 #%% 
 import pandas as pd
 import numpy as np
-from datetime import datetime
+import os
+from datetime import date, datetime
+
+pd.options.mode.chained_assignment = None  # default='warn'
 
 # %%
 
-# Read file
-df = pd.read_csv("responses.csv")
+# Create results directory if not present
+if not os.path.isdir('results'):
+	os.mkdir('results')
 
-cols = df.shape[1]
-no_of_events = (cols - 3) // 6
-
-if (cols - 3) % 6 != 0:
-	print("SHEET STRUCTURE ERROR.")
-
-# %%
-# Name of columns
-colnames = ['timestamp', 'name', 'institute'] + [f"e{i}t{j}" if j < 6 else f"e{i}drive" for i in range(1, no_of_events + 1) for j in range(1, 7)]
-
-df.columns = colnames
-
-df
-
-# %%
-# Drop drive link columns
-drives = [f"e{i}drive" for i in range(1, no_of_events + 1)]
-df.drop(columns=drives, inplace=True)
-
-df
-
-# %%
-
-df['timestamp'] = pd.to_datetime(df['timestamp']).dt.tz_localize('Asia/Kolkata')
-df
-
-# %%
-
-eventdate = "2021-06-05"
-eventdate = datetime.fromisoformat(eventdate)
-
-mask = (df['timestamp'].dt.date == eventdate.date())
-
-df = df.loc[mask]
-
-df
-# %%
-def avg_calc(row, event=1):
+def ao5_calc(row, event=1):
 	indexes = [f"e{event}t{i}" for i in range(1, 6)]
 	times = [row[index] for index in indexes]
 
@@ -72,47 +39,97 @@ def avg_calc(row, event=1):
 		return round(tt/3.0,2)
 
 
-# %%
-# Compute A05
-for i in range(1, no_of_events + 1):
-	df[f"e{i}ao5"] = df.apply(lambda row: avg_calc(row, i), axis=1)
-
-df
-
-# %%
-
-df.sort_values('e1ao5', inplace=True)
-# %%
-df
-# %%
-results = df[['e1ao5', 'name', 'institute']]
-results.columns = ['AO5', 'Name', 'Institute']
-results.dropna(inplace=True)
-
-results
-# %%
-results.to_csv("teste1.tsv", index=False, sep='\t')
-# %%
-
 def whatsapp_string(row):
 	return f"• {row['AO5']:.2f} - {row['Name']} - {row['Institute']}"
 
-def markdown_string(row):
-	return f"| {row['AO5']:.2f} | {row['Name']} | {row['Institute']} |"
-
-results['whatsapp_str'] = results.apply(whatsapp_string, axis=1)
-results['markdown_str'] = results.apply(markdown_string, axis=1)
-
-results
 # %%
-for i in range(len(results)):
-	print(results.iloc[i]['whatsapp_str'])
+def scores(responses="responses.csv", eventdate=date.today().isoformat()):
+	
+	# Read responses file
+	df = pd.read_csv(responses)
 
-print()
-print("Markdown\n")
-print("| Average | Name | Institute |")
-print("| :---: | --- | --- |")
-for i in range(len(results)):
-	print(results.iloc[i]['markdown_str'])
+	# Extract number of columns
+	cols = df.shape[1]
 
+	# Compute number of events
+	no_of_events = (cols - 3) // 6
+	
+	# If division remainder is not 0, sheet structure has changed
+	if (cols - 3) % 6 != 0:
+		print("SHEET STRUCTURE ERROR.")
+
+	# Set column names
+	# Format: timestamp, name, institute
+	# And then for each event i: eit1, eit2, eit3, eit4, eit5, eidrive
+	colnames = ['timestamp', 'name', 'institute'] + [f"e{i}t{j}" if j < 6 else f"e{i}drive" for i in range(1, no_of_events + 1) for j in range(1, 7)]
+
+	df.columns = colnames
+
+	# Drop drive link columns
+	drives = [f"e{i}drive" for i in range(1, no_of_events + 1)]
+	df.drop(columns=drives, inplace=True)
+
+	# Set timestamp as datetime type, timezone-naive
+	# To make timezone aware: df['timestamp].dt.tz_localize('Asia/Kolkata)
+	df['timestamp'] = pd.to_datetime(df['timestamp'])
+
+	# Convert event date to datetime type
+	eventdatestr = eventdate
+	eventdate = datetime.fromisoformat(eventdate)
+
+	# Keep only eventdate data in df
+	mask = (df['timestamp'].dt.date == eventdate.date())
+	df = df.loc[mask]
+
+	if df.empty:
+		print("No data for given event date. Supply a valid date.")
+		return None
+	
+	# Compute AO5 for each event
+	for i in range(1, no_of_events + 1):
+		df[f"e{i}ao5"] = df.apply(lambda row: ao5_calc(row, i), axis=1)
+
+
+	# Results Printing and Storing
+	for i in range(no_of_events):
+		eventname = f"e{i+1}ao5"
+
+		# Sort by event
+		df.sort_values(eventname, inplace=True)
+
+		# Extract event specific results
+		results = df[[eventname, 'name', 'institute']]
+
+		# Set new column names
+		results.columns = ['AO5', 'Name', 'Institute']
+		
+		# Drop NA values
+		results.dropna(inplace=True)
+
+		# Save event results in TSV (Tab Separated Values) file
+		resultspath = f"results/{eventdatestr}_event_{i+1}.tsv"
+		results.to_csv(resultspath, index=False, sep='\t')
+		print(f"Event {i+1} Results saved to {resultspath}\n")
+
+		# Compute strings
+		markdown_str = results.to_markdown(index=False, colalign=('center', 'left', 'left'))
+		results['whatsapp_str'] = results.apply(whatsapp_string, axis=1)
+
+		# Print Strings
+		print(f"WhatsApp String - Event {i+1}\n")
+		for j in range(len(results)):
+			print(results.iloc[j]['whatsapp_str'])
+
+		print()
+		print(f"Markdown String - Event {i+1}\n")
+		print(markdown_str)
+
+		print("\n\n--------------------------------\n\n")
+
+	print("COMPLETE")
+
+		
+
+# %%
+scores(eventdate='2021-06-26')
 # %%
